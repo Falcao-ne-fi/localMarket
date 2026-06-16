27b7f6d46470b8428ca003d561301901d4658d9b
Falcao <henrique.m.falcao67@gmail.com>
2026-06-16 11:01:52 -0300

    Add files via upload

--- src/Login/script.js
+++ src/Login/script.js
@@ -4,8 +4,13 @@ function login(){
     var senha = $("#senha").val()
 
     if(nome && senha && nome === "admin" && senha === "123456"){
-
-
+        const user = {
+            name: nome,
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random() * 100000)
+        }
+        localStorage.setItem("usuario", JSON.stringify(user))
+        window.location.href = "../Loja"
     }else{
         document.getElementById("error-modal").style.display = "flex"
         document.getElementById("nome").style.border = "2px solid red"
@@ -15,5 +20,16 @@ function login(){
 }
 
 function showPassword(){
+    var inputSenha = document.querySelector('#senha')
+    var img_eye = document.querySelector('#eye')
+
+    if(inputSenha.getAttribute("type") === "password"){
+        inputSenha.setAttribute("type", "text")
+        img_eye.setAttribute("src", "../../public/oio_fechado.png")
+    }else{
+        inputSenha.setAttribute("type", "password")
+        img_eye.setAttribute("src", "../../public/oio_aberto.png")
+    }
+
 
 }
