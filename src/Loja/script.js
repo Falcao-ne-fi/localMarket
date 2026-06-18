44991abb3001395d91916060e57dd35c3b4727cc
Falcao <henrique.m.falcao67@gmail.com>
2026-06-18 12:24:40 -0300

    Add files via upload

--- src/Loja/script.js
+++ src/Loja/script.js
@@ -1,10 +1,46 @@
 let produtos
 
 window.onload = function(){
-    var storedUser = localStorage.getItem("usario")
+    var storedUser = localStorage.getItem("usuario")
     var user = JSON.parse(storedUser)
     var dataEntrada = new Date(user.dataEntrada)
 
-    let produtos = "coco"
-    
+    var dataFormatada = dataEntrada.toLocaleString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+    })
+
+    document.getElementById("user").textContent = user.name
+    document.getElementById("perfil").textContent = dataFormatada
+    document.getElementById("idPerfil").textContent = user.id
+   
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch("../Dados/data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            produtos = data
+
+            const produtosContainer = document.getElementById("produtos-container")
+
+            produtos.forEach((produto, index) => {
+                const card = document.createElement("div")
+                card.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <img src="${produto.imagem}" class="card-img-top" alt="${produto.desc}">
+                    <div class="card-body">
+                        <h5 class="card-title">${produto.desc}</h5>
+                        <p class="card-text">${produto.valor.toFixed(2)}</p>
+                        <a href="#" class="btn btn-primary" data-indice="${index}">Go somewhere</a>
+                    </div>
+                </div>
+                `
+
+                produtosContainer.appendChild(card)
+            })
+        }).catch((error) => console.log("Erro ao carregar dados", error))
+})
