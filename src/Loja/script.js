91e47845656b2d0bd2a5f8573bbee5113d5386bd
Falcao <henrique.m.falcao67@gmail.com>
2026-06-18 09:26:56 -0300

    Add files via upload

---
+++ src/Loja/script.js
@@ -0,0 +1,10 @@
+let produtos
+
+window.onload = function(){
+    var storedUser = localStorage.getItem("usario")
+    var user = JSON.parse(storedUser)
+    var dataEntrada = new Date(user.dataEntrada)
+
+    let produtos = "coco"
+    
+}
