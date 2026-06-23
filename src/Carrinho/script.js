450f44b1a5d76126b1ba50cdef0b0f77548dcd2f
Falcao <henrique.m.falcao67@gmail.com>
2026-06-22 21:50:05 -0300

    Add files via upload

--- src/Carrinho/script.js
+++ src/Carrinho/script.js
@@ -1,34 +1,35 @@
 
-$(document).ready(function() {
-    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
-    const listElement = $('#lista')
+$(document).ready(function() { // ".ready" executa a função quando o html terminar de carregar
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [] //pega a key do carrinho, transforma em objeto, e atribui à variavel criada
+    const listElement = $('#lista') // conecta as variaveis listElement e totalElement com os elemntos no HTML, sendo eles a div e o h3    (sintax diferente, igual document.getElementById)
     const totalElement = $("#total")
 
-    function exibirCarrinho(){
-        listElement.empty()
+    function exibirCarrinho(){ //função para mostrar o carrinho
+        listElement.empty() //limpa a lista, removendo todos os "filhos"
         let totalPreco = 0
 
-        $.each(carrinho, function(index, item){
-            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.preco.toFixed(2)}`)
+        $.each(carrinho, function(index, item){ //vai percorrer todos os itens no carrinho
+            const listItem = $("<li>").text(`${item.desc} - Preço: R$${item.valor.toFixed(2)}`) // cria um <li>, e adiciona um texto 
 
-            const removeButton = $("<button>").text("❌").css("margin-left", "10px").click(function(){
-                removerItem(index)
+            const removeButton = $("<button>").text("❌").css("margin-left", "10px").click(function(){ //cria um botao com o texto "❌", atribui um css, e a função quando clicar
+                removerItem(index) //executa a função removerItem de acordo com o index do produto
             })
 
-            listItem.append(removeButton)
-            listaElement.append(listItem)
+            listItem.append(removeButton) //adiciona o botao no html
+            listElement.append(listItem) // adiciona o item no html
 
-            totalPreco += item.preco
+            totalPreco += item.valor // adiciona o preço do produto ao preço total
             //appends filhos aqui
         })
 
-        totalElement.text(`Total: $${totalPreco.toFixed(2)}`)
+        totalElement.text(`Total: $${totalPreco.toFixed(2)}`) //muda o texto do html para mopstrar o reço total
     }
+    exibirCarrinho()
 
-    function removerItem(index){
-        carrinho.splice(index, 1)
-        localStorage.setItem("carrinho", JSON.stringify(carrinho))
-        exibirCarrinho()
+    function removerItem(index){ //funçao pra remover o item do carrinho, precisa do index de parametro
+        carrinho.splice(index, 1) //remove o produto de acordo com o index
+        localStorage.setItem("carrinho", JSON.stringify(carrinho)) //salva o novo carrinho
+        exibirCarrinho()// carrega o carrinho denovo
     }
 
     //funcao de removerItem
